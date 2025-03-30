2b9a020f9276feab9089c377bb45b32b8190c1cb
puctom <robakibobaki@op.pl>
2025-03-30 21:37:36 +0200

    fix order of entries in objects

--- src/Jq/Utils.hs
+++ src/Jq/Utils.hs
@@ -3,9 +3,10 @@ import Jq.Json
 
 removeDuplicateKeys :: [(String, JSON)] -> [(String, JSON)]
 removeDuplicateKeys entries = foldr
-                        (\(key, val) acc -> 
-                            if key `elem` map fst acc 
-                            then 
-                                acc 
-                            else (key, val) : acc
+                        (\ (key, val) acc ->
+                            if key `elem` map fst acc
+                            then
+                                filter (\(x,_) -> x == key) acc ++ (filter (\(x,_) -> x /= key) acc)
+                            else 
+                                (key, val) : acc
                         ) [] entries
